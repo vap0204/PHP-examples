59db6eb1089b4d98347653a3cc876a9752923938
Vesela Apostolova <100559743+vap0204@users.noreply.github.com>
2023-03-15 16:34:10 +0200

    Update kontrolna.php

--- kontrolna.php
+++ kontrolna.php
@@ -1,15 +1,9 @@
-<!--Na  Php da se napravi indeksen masiv ot pone 3 el vseki vseki ot el 
-na indeksniq masiv sushto predstavlqva masiv ot 3 stoinosti koi to dostupvame s kluchove marka na avto bus reg nomer i liniq po koqto se dviji sled koeto v web straniocata da se vizualizirat v nomeriran spisuk !-->
-
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Vesela Apostolova_2101681070_Kontrolna</title>
-        <style>
-            
-        </style>
-    </header>
+        <title>Kontrolna</title>
+    </head>
     <body>
         <?php
          $array=array(array("brand"=>"Mercedes","number"=>"РВ2322КМ","line"=>"11"),
@@ -18,8 +12,7 @@ na indeksniq masiv sushto predstavlqva masiv ot 3 stoinosti koi to dostupvame s 
          echo"<ol>";
          foreach($array as $item){
           echo "<li><u>".$item["brand"]."</u>". " ".$item["number"]." ".$item["line"]."</li>";
-        
-
+       
          }
          echo"</ol>";
         ?>
